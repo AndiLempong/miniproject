5059acba703bbc6e4c4982d329cf6cc283fbf1d6
AndiLempong <kpes2901@gmail.com>
2023-09-19 06:42:41 +0700

    update

---
+++ application/views/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PENDAFTARAN ONLINE</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <style>
+        .background {
+            background-image: url('https://foto.data.kemdikbud.go.id/getImage/20328986/12.jpg');
+            background-size: cover;
+        }
+        .background-image-black {
+        height: 100vh;
+        background-color: rgb(0, 0, 0, 0.4);
+        }
+    </style>
+
+</head>
+<body class="background">
+    <div class="background-image-black" >
+        <h1 style="text-align: center; color:white;">𝐏𝐄𝐍𝐃𝐀𝐅𝐓𝐀𝐑𝐀𝐍</h1>
+        <hr>
+        <center><img src="https://binusasmg.sch.id/ppdb/logobinusa.png" alt="" width="220px" height="200px">
+        
+            <h2 style="text-align: center; color:white;">𝐒𝐌𝐊 𝐁𝐈𝐍𝐀 𝐍𝐔𝐒𝐀𝐍𝐓𝐀𝐑𝐀 𝐒𝐄𝐌𝐀𝐑𝐀𝐍𝐆</h2>
+            <h3 style="text-align:center; color:red;">"Cerdas,Santun,Berbudi Luhur"</h3>
+            <p style="text-align:center; color:white;">Jl. Kemantren Raya No.5, RT.02/RW.04, Wonosari, Kec. Ngaliyan, Kota Semarang, Jawa Tengah 5018 </p>
+
+        <a class="btn btn-primary" href="auth">Login</a>
+    </center>
+</div>
+
+</body>
+</html
